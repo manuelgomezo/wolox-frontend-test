e263d314659b782939526b12ff0bfc6081f47644
Manuel Gomez <manuel@experimentality.co>
2020-09-02 23:20:17 -0500

    Added locale buttons on Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './Footer.scss';
+import { useTranslation } from 'react-i18next';
 import config from 'config';
+import './Footer.scss';
 
 const Footer = () => {
+  const { i18n } = useTranslation();
+
   return (
     <div className="footer">
       <div class="footer-background">
@@ -11,6 +14,15 @@ const Footer = () => {
 
       <div className="footer__logo">
         <img src={config.IMAGE_PROVIDER + 'icons/Ic_wolox_footer.svg'} alt="WOLOX" />
+      </div>
+
+      <div className="footer__locales">
+        <button onClick={() => i18n.changeLanguage('es')}>
+          <img src={config.IMAGE_PROVIDER + 'images/es.png'} alt="WOLOX ES Locale" />
+        </button>
+        <button onClick={() => i18n.changeLanguage('en')}>
+          <img src={config.IMAGE_PROVIDER + 'images/en.png'} alt="WOLOX EN Locale" />
+        </button>
       </div>
     </div>
   );
