d0ea0f7ee5cb88b5472308352d493dd75bdee73f
Manuel Gomez <manuel@experimentality.co>
2020-09-09 13:20:03 -0500

    Index loading, Card Force HTTPS images.

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import classnames from 'classnames';
 import './Card.scss';
 
+const forceHttps = (url) => {
+  return url.replace('http://', 'https://');
+};
+
 const Card = ({ className, title, image, children }) => {
   return (
     <div className={classnames('card', className)}>
       <div className="card__container">
-        <div className="card__image">{image && <img src={image} alt={title} />}</div>
+        <div className="card__image">{image && <img src={forceHttps(image)} alt={title} />}</div>
         <div className="card__content">
           <h2 className="card__title">{title}</h2>
           {children}
